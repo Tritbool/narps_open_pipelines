3b3983b9e75a9d7877e78a9d942fc1c04a350b6e
BuildTools <gauthier.lyan@digital4better.com>
2024-04-05 22:22:05 +0200

    trying on apple silicon

--- examples/Q6O0_debug.py
+++ examples/Q6O0_debug.py
@@ -11,15 +11,15 @@ import pickle
 
 def main():
 
-    matlab_cmd = '/opt/spm12-r7219/run_spm12.sh /opt/matlabmcr-2010a/v713/ script'
+    matlab_cmd = '/opt/narps/spm12_2023b/run_spm12.sh /opt/MATLAB/MATLAB_Runtime/R2023b script'
     spm.SPMCommand.set_mlab_paths(matlab_cmd=matlab_cmd, use_mcr=True)
     team_ID = "Q6O0"
     # Important directories
     ## exp_dir : where the data are stored
-    exp_dir = '/home/tritbool/workspace/narps_open_pipelines/data/original/ds001734/'
+    exp_dir = '/Users/michel/workspace/narps_open_pipelines/data/original/ds001734/'
 
     ## result_dir : where the intermediate and final results will be store
-    result_dir = "/home/tritbool/workspace/narps_open_pipelines/data/reproduced/"
+    result_dir = "/Users/michel/workspace/narps_open_pipelines/data/reproduced/"
 
     ## working_dir : where the intermediate outputs will be store
     working_dir = result_dir + f"NARPS-{team_ID}-reproduced/intermediate_results/"
